009d8464b574db1f0d52051af83254b30d17fe33
Willem Van Onsem <yourfriends@hapytex.eu>
2024-10-25 12:46:52 +0200

    commaspec

--- test/Text/CommaSpec.hs
+++ test/Text/CommaSpec.hs
@@ -1,6 +1,12 @@
 module Text.CommaSpec where
 
 import Test.Hspec(Spec, describe, it, shouldBe)
+import Text.Comma(comma)
 
 spec :: Spec
-spec = describe "isWordChar" $ pure ()
+spec = describe "comma" $ do
+    it "comma" $ do
+      comma [] `shouldBe` ""
+      comma ["red"] `shouldBe` "red"
+      comma ["red", "green"] `shouldBe` "red, and green"
+      comma ["red", "green", "blue"] `shouldBe` "red, green, and blue"
